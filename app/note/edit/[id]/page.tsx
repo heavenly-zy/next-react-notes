c55f474cd30a6a0470a0a5a67747dbf2784ec207
ClariS <1457715339@qq.com>
2024-06-10 23:37:17 +0800

    feat: 笔记的新增 & 编辑界面

---
+++ app/note/edit/[id]/page.tsx
@@ -0,0 +1,29 @@
+import NoteEditor from '@/components/NoteEditor';
+import { getNote } from '@/libs/redis';
+import { sleep } from '@/utils';
+
+export default async function EditPage({ params }: { params: { id: string } }) {
+  const noteId = params.id;
+  const note = await getNote<Note>(noteId);
+
+  // 让效果更明显
+  await sleep(5000);
+
+  if (note === null) {
+    return (
+      <div className="note--empty-state">
+        <span className="note-text--empty-state">
+          Click a note on the left to view something! 🥺
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <NoteEditor
+      noteId={noteId}
+      initialTitle={note.title}
+      initialBody={note.content}
+    />
+  );
+}
