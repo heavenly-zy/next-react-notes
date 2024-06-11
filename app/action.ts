c8d1aeb2facd1014b50ac323c8b3a755e2e1f856
ClariS <1457715339@qq.com>
2024-06-11 23:20:18 +0800

    fix: 清除路由缓存

--- app/action.ts
+++ app/action.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from 'next/navigation';
 import { addNote, updateNote, delNote } from '@/libs/redis';
+import { revalidatePath } from 'next/cache';
 
 export async function saveNote(title: string, body: string, noteId?: string) {
   const data = JSON.stringify({
@@ -12,14 +13,17 @@ export async function saveNote(title: string, body: string, noteId?: string) {
 
   if (noteId) {
     updateNote(noteId, data);
+    revalidatePath('/', 'layout');
     redirect(`/note/${noteId}`);
   } else {
     const res = await addNote(data);
+    revalidatePath('/', 'layout');
     redirect(`/note/${res}`);
   }
 }
 
 export async function deleteNote(noteId: string) {
   delNote(noteId);
+  revalidatePath('/', 'layout');
   redirect('/');
 }
