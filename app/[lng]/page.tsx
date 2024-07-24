fe90979f99fe7db03d88369a5f38d31aa21be4a8
ClariS <1457715339@qq.com>
2024-07-25 00:13:48 +0800

    feat: react-i18next - 路由处理

---
+++ app/[lng]/page.tsx
@@ -0,0 +1,13 @@
+export default async function Page({
+  params: { lng },
+}: {
+  params: { lng: string };
+}) {
+  return (
+    <div className="note--empty-state">
+      <span className="note-text--empty-state">
+        Click a {lng} note on the left to view something! 🥺
+      </span>
+    </div>
+  );
+}
