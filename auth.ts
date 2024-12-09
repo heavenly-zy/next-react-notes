2fec5f72b8f4274c39dab7a8d2aa7025d1497854
ClariS <1457715339@qq.com>
2024-12-09 22:58:14 +0800

    chore: 添加证书

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+// import GitHub from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { addUser, getUser } from '@/libs/prisma';
 
@@ -40,7 +40,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         return user as any;
       },
     }),
-    GitHub,
+    // GitHub,
   ],
   pages: {
     signIn: '/auth/signin',
@@ -58,9 +58,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return token;
     },
     async session({ session, token }) {
-      (session.user as any).userId = token.userId;
+      if (token.userId) {
+        (session.user as any).userId = token.userId
+      }
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      else if (new URL(url).origin === baseUrl) return url
+      if (baseUrl.indexOf(":3000") > -1) return url
+      return baseUrl
+    }
   },
   trustHost: true
 });
